fb032657c0b13b17c06680d8dc99652912272eb2
KubaBaniak <kuba121201@gmail.com>
2023-03-22 19:35:53 +0100

    Displaying pie chart with file extensions

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,14 @@
+use std::fs::File;
+use std::mem::size_of;
 use std::path::Path;
 use std::collections::HashMap;
 use std::f32::consts::PI;
+
+#[derive(Debug)]
+struct FileProb {
+    ext: String,
+    prob: f32,
+}
 
 
 fn main() {
@@ -13,10 +21,11 @@ fn main() {
     let mut file_ext: HashMap<String, usize> = HashMap::new();
     get_all_extensions(path, &mut file_ext);
     print_hm(&file_ext);
-    let k = vec!['$', '*', '@', 'x'];
-    let v = vec![0.25, 0.125, 0.125, 0.125];
-    let r = 8;
-    pie_chart(&k, &v, r);
+    println!("{}", change_color_text(226, "siema"));
+    default_color_text();
+    println!("SIEEMA");
+
+    ext_to_pie_chart(&file_ext);
 }
 
 
@@ -59,26 +68,60 @@ fn print_hm(map: & HashMap<String, usize>) {
     }
 }
 
-fn s(k: &Vec<char>, v: &Vec<f32>, a: f32) -> char {
-    if v.is_empty() {
-        return ' '
-    } else if a<v[0] {
-        return k[0]
-    }
-    return s(&(k[1..].to_vec()), &(v[1..].to_vec()), a-v[0])
-}
 
-fn pie_chart(k: &Vec<char>, v: &Vec<f32>, r: i32) {
-    for y in (-r)..r {
-        let mut t = String::new();
-        for x in (-r)..r {
+fn print_pie_chart(k: &Vec<char>, v: &Vec<f32>, r: i32) {
+
+    fn s(k: &Vec<char>, v: &Vec<f32>, a: f32) -> char {
+        if v.is_empty() {
+            return ' '
+        }
+        if a<v[0] {
+            return k[0]
+        }
+        s(&(k[1..].to_vec()), &(v[1..].to_vec()), a-v[0])
+    }
+
+    let range = (-r)..r;
+    for y in range.clone() {
+        let mut pie = String::new();
+        for x in range.clone() {
             if x*x + y*y < r*r {
                 let a = (y as f32).atan2(x as f32) / PI /2.0+0.5;
-                t.push(s(k, v, a));
+                pie.push(s(k, v, a));
             } else {
-                t.push(' ')
+                pie.push(' ')
             }
         }
-        println!("{}", t)
+        println!("{}", pie)
     }
+}
+
+fn change_color_text(color: usize, text: &str) -> String {
+    format!("\x1b[38;5;{}m{}", color, text)
+}
+
+fn default_color_text() {
+    println!("\x1b[0m");
+}
+
+fn ext_to_pie_chart(map: &HashMap<String, usize>) {
+    let n: usize = map.values().sum();
+    
+    let mut parts: Vec<FileProb> = Vec::new();
+
+    for (key, value) in map.iter() {
+        parts.push(FileProb { ext: key.clone(), prob: *value as f32/n as f32 });
+    }
+
+    println!("{:?}\n{}", parts, parts.len());
+
+    let mut k: Vec<char> = Vec::new();
+    for i in 65..65+parts.len() {
+        k.push( (i as u8) as char)
+    }
+    println!("{:?}", k);
+    let v = parts.into_iter().map(|p| p.prob).collect();
+
+    let r = 15;
+    print_pie_chart(&k, &v, r);
 }
