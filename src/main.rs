4f9a9e74718c0ee749af231dfb4feecc3bc50e37
KubaBaniak <kuba121201@gmail.com>
2023-03-22 16:21:03 +0100

    Pie chart implementation

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::path::Path;
 use std::collections::HashMap;
+use std::f32::consts::PI;
 
 
 fn main() {
@@ -12,6 +13,10 @@ fn main() {
     let mut file_ext: HashMap<String, usize> = HashMap::new();
     get_all_extensions(path, &mut file_ext);
     print_hm(&file_ext);
+    let k = vec!['$', '*', '@', 'x'];
+    let v = vec![0.25, 0.125, 0.125, 0.125];
+    let r = 8;
+    pie_chart(&k, &v, r);
 }
 
 
@@ -51,5 +56,29 @@ fn get_all_extensions(path: & Path, map: &mut HashMap<String, usize>) {
 fn print_hm(map: & HashMap<String, usize>) {
     for (key, value) in map.iter() {
         println!("{key}: {value}");
+    }
+}
+
+fn s(k: &Vec<char>, v: &Vec<f32>, a: f32) -> char {
+    if v.is_empty() {
+        return ' '
+    } else if a<v[0] {
+        return k[0]
+    }
+    return s(&(k[1..].to_vec()), &(v[1..].to_vec()), a-v[0])
+}
+
+fn pie_chart(k: &Vec<char>, v: &Vec<f32>, r: i32) {
+    for y in (-r)..r {
+        let mut t = String::new();
+        for x in (-r)..r {
+            if x*x + y*y < r*r {
+                let a = (y as f32).atan2(x as f32) / PI /2.0+0.5;
+                t.push(s(k, v, a));
+            } else {
+                t.push(' ')
+            }
+        }
+        println!("{}", t)
     }
 }
